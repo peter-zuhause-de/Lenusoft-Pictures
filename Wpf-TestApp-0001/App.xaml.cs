a9e223878f3c7ef5fdabf4ad5d016d4091c45f22
peter-zuhause-de <peter.lenuweit@live.de>
2025-12-24 23:39:33 +0100

    Projektdateien hinzufügen.

---
+++ Wpf-TestApp-0001/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace Wpf_TestApp_0001
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+       
+    }
+
+}
